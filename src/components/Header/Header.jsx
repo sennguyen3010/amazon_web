acbe5a1aad19686d804b211d46a684757e928a9a
sennguyen3010 <kimsen3077@gmail.com>
2023-02-23 14:44:52 +0700

    create project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <NavLink className="text-white">Amazon</NavLink>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here ..."
+                  aria-label="Search Product Here ..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <NavLink className="header-upper-links_item">
+                    <img src="img/compare.svg" alt="compare" />
+                    <p>
+                      Compare <br /> Products
+                    </p>
+                  </NavLink>
+                </div>
+
+                <div>
+                  <NavLink className="header-upper-links_item">
+                    <img src="img/wishlist.svg" alt="Favourite" />
+                    <p>
+                      Favourite <br /> Wishlist
+                    </p>
+                  </NavLink>
+                </div>
+
+                <div>
+                  <NavLink className="header-upper-links_item">
+                    <img src="img/user.svg" alt="user" />
+                    <p>
+                      Login in <br /> My Account
+                    </p>
+                  </NavLink>
+                </div>
+
+                <div>
+                  <NavLink className="header-upper-links_item">
+                    <img src="img/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p>$ 500</p>
+                    </div>
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button
+                      className="menu-links_text btn btn-secondary dropdown-toggle d-flex gap-15 align-items-center"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img src="img/menu.svg" alt="menu" />
+                      <span className="me-5 d-inline-block">
+                        SHOP CATEGORIES
+                      </span>
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <NavLink className="dropdown-item text-white" to="/">
+                          Action
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink className="dropdown-item text-white" to="/">
+                          Another action
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink className="dropdown-item text-white" to="/">
+                          Something else here
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink className="menu-links_text" to="/">
+                      HOME
+                    </NavLink>
+                    <NavLink className="menu-links_text" to="/">
+                      OUR STORE
+                    </NavLink>
+                    <NavLink className="menu-links_text" to="/">
+                      BLOGS
+                    </NavLink>
+                    <NavLink className="menu-links_text" to="/">
+                      CONTACT
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import './assets/scss/style.scss';
+
+import {
+  Navigate,
+  Route,
+  Routes,
+  unstable_HistoryRouter as HistoryRouter,
+} from 'react-router-dom';
+
+import { createBrowserHistory } from 'history';
+import Home from './pages/Home';
+import About from './pages/About';
+import UserTemplate from './templates/user/UserTemplate';
+import { store } from './redux/configStore';
+
+export const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <HistoryRouter history={history}>
+      <Routes>
+        <Route path="" element={<UserTemplate />}>
+          <Route index element={<Home />}></Route>
+          <Route path="about" element={<About />}></Route>
+        </Route>
+      </Routes>
+    </HistoryRouter>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
